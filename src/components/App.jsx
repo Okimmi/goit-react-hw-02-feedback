1fb4baa7e7cbbc87b7e01cbad0fa9ac0f48a3de7
Olena <petrova1512@gmail.com>
2023-08-22 20:29:24 +0200

    hw part1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Feedback } from './Feedback/Feedback';
+
+export class App extends Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+
+  clickBtn = e => {
+    this.setState(prevState => ({
+      [e.target.name]: prevState[`${e.target.name}`] + 1,
+    }));
+  };
+
+  countTotalFeedback = () =>
+    Object.values(this.state).reduce(
+      (previousValue, item) => previousValue + item,
+      0
+    );
+
+  countPositiveFeedbackPercentage = () =>
+    Math.round((this.state.good / this.countTotalFeedback()) * 100);
+
+  render() {
+    return (
+      <div>
+        <Feedback
+          state={this.state}
+          clickBtn={this.clickBtn}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
+      </div>
+    );
+  }
+}
